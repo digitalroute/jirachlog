a25be8007e0bb29dd5a506e64072484e4b8d79be
Marcus Johansson <marcus.johansson@digitalroute.com>
2019-07-11 09:59:42 +0200

    added jirachlog and setuptools to upload to pipy

---
+++ README.md
@@ -0,0 +1,15 @@
+# jirachlog
+
+## Introduction
+
+Creates a basic changelog using git and Jira
+
+## Prerequisites
+
+Make sure you have Python3.
+
+## Installation
+
+Run command:
+
+    pip3 install jirachlog
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+exec(open('jirachlog/ver.py').read())
+
+with open('README.md') as file:
+    long_description = file.read()
+
+setup(name='jirachlog',
+    version=__version__,
+    description='Creates a basic changelog using git and Jira',
+    author="Marcus Johansson",
+    author_email="polarn@gmail.com",
+    long_description=long_description,
+    license='APACHE',
+    url='https://github.com/digitalroute/jirachlog',
+    packages=['jirachlog'],
+    scripts=['bin/jirachlog'],
+    zip_safe=False,
+    install_requires=[
+        'jira>=2.0.0'
+    ],
+)
